01964f0080530e683049421958ec609317ed1b19
monishjodha <monish.21jics202@jietjodhpur.ac.in>
2023-06-06 12:26:13 +0530

    data transfering and linking is been done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,75 @@
 function sub(){
-   const val=document.getElementById("n").value;
-   localStorage.setItem("VALUE", val);
+   const val1=document.getElementById("i1").value;
+   localStorage.setItem("VALUE1", val1);
    
+   const val2=document.getElementById("i2").value;
+   localStorage.setItem("VALUE2", val2);
+
+   const val3=document.getElementById("i3").value;
+   localStorage.setItem("VALUE3", val3);
+
+   const val4=document.getElementById("i4").value;
+   localStorage.setItem("VALUE4", val4);
+
+   const val5=document.getElementById("i5").value;
+   localStorage.setItem("VALUE5", val5);
+
+   const val6=document.getElementById("i6").value;
+   localStorage.setItem("VALUE6", val6);
+
+   const val7=document.getElementById("i7").value;
+   localStorage.setItem("VALUE7", val7);
+
+   const val8=document.getElementById("i8").value;
+   localStorage.setItem("VALUE8", val8);
+
+
+   const val9=document.getElementById("i9").value;
+   localStorage.setItem("VALUE9", val9);
+
+   const val10=document.getElementById("i10").value;
+   localStorage.setItem("VALUE10", val10);
+
+   const val11=document.getElementById("i11").value;
+   localStorage.setItem("VALUE11", val11);
+
+   const val12=document.getElementById("i12").value;
+   localStorage.setItem("VALUE12", val12);
+
+   const val13=document.getElementById("i13").value;
+   localStorage.setItem("VALUE13", val13);
+
+   const val14=document.getElementById("i14").value;
+   localStorage.setItem("VALUE14", val14);
+
+   const val15=document.getElementById("i15").value;
+   localStorage.setItem("VALUE15", val15);
+
+   const val16=document.getElementById("i16").value;
+   localStorage.setItem("VALUE16", val16);
+
+   const val17=document.getElementById("i17").value;
+   localStorage.setItem("VALUE17", val17);
+
+   const val18=document.getElementById("i18").value;
+   localStorage.setItem("VALUE18", val18);
+
+   const val19=document.getElementById("i19").value;
+   localStorage.setItem("VALUE19", val19);
+
+   const val20=document.getElementById("i20").value;
+   localStorage.setItem("VALUE20", val20);
+
+   const val21=document.getElementById("i21").value;
+   localStorage.setItem("VALUE21", val21);
+
+   const val22=document.getElementById("i22").value;
+   localStorage.setItem("VALUE22", val22);
+
+   const val23=document.getElementById("i23").value;
+   localStorage.setItem("VALUE", val23);
 
 }
+function subb(){
+   const val=document.getElementById("n").value;
+   localStorage.setItem("VALUE", val);}
--- src/result.js
+++ src/result.js
@@ -1,4 +1,73 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const v= localStorage.getItem('VALUE');
-    document.getElementById("put").innerHTML= v;
+    const v1= localStorage.getItem('VALUE1');
+    document.getElementById("set").innerHTML= v1;
+
+    const v2= localStorage.getItem('VALUE2');
+    document.getElementById("detail").innerHTML= v2;
+
+    const v3= localStorage.getItem('VALUE3');
+    document.getElementById("g3").innerHTML= v3;
+
+    const v4= localStorage.getItem('VALUE4');
+    document.getElementById("g4").innerHTML= v4;
+
+    const v5= localStorage.getItem('VALUE5');
+    document.getElementById("g5").innerHTML= v5;
+
+    const v6= localStorage.getItem('VALUE6');
+    document.getElementById("g6").innerHTML= v6;
+
+    const v0= localStorage.getItem('VALUE6');
+    document.getElementById("g0").innerHTML= v0;
+
+    const v7= localStorage.getItem('VALUE7');
+    document.getElementById("g7").innerHTML= v7;
+
+    const v8= localStorage.getItem('VALUE8');
+    document.getElementById("g8").innerHTML= v8;
+
+    const v9= localStorage.getItem('VALUE9');
+    document.getElementById("g9").innerHTML= v9;
+
+    const v10= localStorage.getItem('VALUE10');
+    document.getElementById("g10").innerHTML= v10;
+
+    const v11= localStorage.getItem('VALUE11');
+    document.getElementById("g11").innerHTML= v11;
+
+    const v12= localStorage.getItem('VALUE12');
+    document.getElementById("g12").innerHTML= v12;
+
+    const v13= localStorage.getItem('VALUE13');
+    document.getElementById("g13").innerHTML= v13;
+
+    const v14= localStorage.getItem('VALUE14');
+    document.getElementById("g14").innerHTML= v14;
+
+    const v15= localStorage.getItem('VALUE15');
+    document.getElementById("g15").innerHTML= v15;
+
+    const v16= localStorage.getItem('VALUE16');
+    document.getElementById("g16").innerHTML= v16;
+
+    const v17= localStorage.getItem('VALUE17');
+    document.getElementById("g17").innerHTML= v17;
+
+    const v18= localStorage.getItem('VALUE18');
+    document.getElementById("g18").innerHTML= v18;
+
+    const v19= localStorage.getItem('VALUE19');
+    document.getElementById("g19").innerHTML= v19;
+
+    const v20= localStorage.getItem('VALUE20');
+    document.getElementById("g20").innerHTML= v20;
+
+    const v21= localStorage.getItem('VALUE21');
+    document.getElementById("g21").innerHTML= v21;
+
+    const v22= localStorage.getItem('VALUE22');
+    document.getElementById("g22").innerHTML= v22;
+
+    const v23= localStorage.getItem('VALUE23');
+    document.getElementById("g23").innerHTML= v23;
 })
